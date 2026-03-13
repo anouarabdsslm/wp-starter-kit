f0e2d158311439a8597d3e2ecdb08d838e61da75
Anouar <anouar.abdessalam@v9.group>
2026-03-13 23:27:40 +0000

    Enhaanc efooter

--- wp-content/mu-plugins/pivotlar-footer-credit.php
+++ wp-content/mu-plugins/pivotlar-footer-credit.php
@@ -5,5 +5,5 @@
  */
 
 add_action('wp_footer', function () {
-    echo '<div style="text-align:center;padding:10px 0;font-size:14px;color:#666;">New Updated:This is github starter kit by pivotlar</div>';
+    echo '<div style="text-align:center;padding:10px 0;font-size:14px;color:#666;">Updated-two:This is github starter kit by pivotlar</div>';
 });
